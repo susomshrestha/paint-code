13d63bc0d10372a6265980204f9c2e1250f389e6
Susom <susomshrestha@gmail.com>
2026-07-14 22:23:35 -0400

    add basename in browser router for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="/paint-code">
 			<App />
 		</BrowserRouter>
 	</StrictMode>,
